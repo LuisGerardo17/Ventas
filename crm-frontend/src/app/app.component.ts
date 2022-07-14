b895399465458b87619e860319957ced4c293f63
LuisGerardo17 <90858992+LuisGerardo17@users.noreply.github.com>
2022-07-14 15:28:38 -0500

    Delete app.component.ts
    anulado

--- crm-frontend/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-//decorador en angular
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-})
-export class AppComponent {
-  title = 'crm-frontend';
-}
